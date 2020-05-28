110f64e76ad795b3bc74c4d80ecf0cba5186796c
Luis Otávio Lourenço de Souza <luisotavioldesouza1@gmail.com>
2020-05-28 16:05:10 -0300

    Animation end, with react-transition-group

--- Animations/src/App.js
+++ Animations/src/App.js
@@ -26,25 +26,30 @@ class App extends Component {
         <h1>React Animations</h1>
         <button className="Button" onClick={() => this.setState(prev => ({ showBlock: !prev.showBlock }))}>Toggle</button>
         <br />
-        <Transition 
-        in={this.state.showBlock} 
-        timeout={1000}
-        mountOnEnter
-        unmountOnExit>
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}>
           {state => (
-          <div style={{ 
-          backgroundColor: 'red', 
-          width: 100, 
-          height: 100, 
-          margin: 'auto',
-          transition: 'opacity 1s ease-out',
-          opacity: state === 'exiting' ? 0 : 1
-         }}></div>
-          ) }
-          
+            <div style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              transition: 'opacity 1s ease-out',
+              opacity: state === 'exiting' ? 0 : 1
+            }}></div>
+          )}
         </Transition>
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
-        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ### React concepts and new stuff study repository
+
+
+- [X] React animations
+- [ ] React Hooks
+- [ ] Usefull libraries
