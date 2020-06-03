1331c4a98b95fb4b2cf29b24e59689bdef1f3864
Luis Otávio Lourenço de Souza <luisotavioldesouza1@gmail.com>
2020-06-03 16:02:16 -0300

    useState and useEffect with http request done

--- React Hooks/src/components/Ingredients/Ingredients.js
+++ React Hooks/src/components/Ingredients/Ingredients.js
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
+  const [ingredientsState, setIngredientsState] = useState([]);
+
+
+  useEffect(()=> {
+    console.log('RENDERING INGREDIENTS', ingredientsState)
+  }, [ingredientsState]);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setIngredientsState(filteredIngredients)
+  }, [])
+
+  
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-hooks-daa0e.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setIngredientsState(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    });
+
+  }
+
+  const removeIngredientHandler = ingredientId => {
+    const ing = ingredientsState.filter(ingredients => ingredients.id !== ingredientId);
+    setIngredientsState(ing)
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
+        <IngredientList ingredients={ingredientsState} onRemoveItem={removeIngredientHandler} />
         {/* Need to add list here! */}
       </section>
     </div>
--- React Hooks/src/components/Ingredients/Search.js
+++ React Hooks/src/components/Ingredients/Search.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props;
+  const [filterState, setFilterState] = useState('');
+
+  useEffect(()=>{
+    const query = filterState.length === 0 ? '' : `?orderBy="title"&equalTo="${filterState}"`;
+    fetch('https://react-hooks-daa0e.firebaseio.com/ingredients.json' + query)
+    .then(response=> response.json())
+    .then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      onLoadIngredients(loadedIngredients)
+    });
+  }, [filterState, onLoadIngredients])
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" 
+          value={filterState} 
+          onChange={event => setFilterState(event.target.value)}/>
         </div>
       </Card>
     </section>
